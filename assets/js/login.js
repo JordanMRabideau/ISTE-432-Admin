98c470f1d63f28fc70aeeff7bb210e517ec46d07
Steffen Barr <smb3402@rit.edu>
2022-12-02 17:28:50 -0500

    admin login

---
+++ assets/js/login.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function xhr(getPost, url, data) {
+  return $.ajax({
+    type: getPost,
+    data: data,
+    dataType: "json",
+    cache: false,
+    async: true,
+    url: url,
+    success: function(data) {
+        window.localStorage.setItem("user", data.user)
+        window.location.href = "./pages/manager.html"
+    },
+    error: function(err, exception) {
+        console.log(err)
+        if (err.status === 401) {
+            alert("Invalid login credentials")
+        } else if (err.status === 502) {
+            alert("Bad gateway")
+        } else {
+            alert("Uncaught exception")
+        }
+    }
+  }).fail(function (err) {
+    console.log(err);
+  });
+}
+
+$(document).ready(function() {
+    window.localStorage.setItem("society", 1) // The society is hardcoded for each site
+
+    $("#login-form").submit(function(e) {
+        e.preventDefault()
+        const data = {
+            auth1: $("#auth1").val(),
+            auth2: $("#auth2").val()
+        }
+
+        xhr("post", "http://localhost:3000/api/admin", data)
+    })
+})
+
