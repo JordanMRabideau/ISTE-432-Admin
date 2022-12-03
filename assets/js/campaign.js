563e26c097ab46dafe59f55b32ff0fc64d91f136
Jordan <jordanmrabideau@gmail.com>
2022-12-02 21:04:43 -0500

    add paper ballot import

--- assets/js/campaign.js
+++ assets/js/campaign.js
@@ -28,7 +28,6 @@ function formatQuestions(questions) {
 
     return -1;
   });
-  console.log(sorted);
   let formatted = [];
 
   sorted.forEach((item) => {
@@ -40,12 +39,15 @@ function formatQuestions(questions) {
       bio: item.bio,
       image_filepath: item.image_filepath,
       vote_count: item.vote_count,
+      choice_placement: item.choice_placement
     };
+    
     // If the question isn't already in the formatted array, create the question and add it
     if (!question) {
       formatted.push({
         question_id: item.question_id,
         question: item.question,
+        question_placement: item.question_placement,
         maximum_selections: item.maximum_selections,
         choices: [],
       });
@@ -60,11 +62,67 @@ function formatQuestions(questions) {
   return formatted;
 }
 
+function validatePaperBallot(formattedBallot, paper) {
+  const ballotInts = paper.map(i => parseInt(i))
+  const ballotId = ballotInts.shift()
+  const selections = []
+  let numFields = 0
+
+  // determine how many fields should be in the csv, throw out any non-compliant rows
+  formattedBallot.forEach(b => {
+    numFields += b.choices.length
+  })
+
+  if (ballotInts.length != numFields) {
+    return []
+  }
+
+  formattedBallot.forEach(b => {
+    const max = b.maximum_selections
+
+    const question = ballotInts.splice(0, b.choices.length)
+    const checked = question.reduce((a, b) => a + b, 0)
+
+    if (checked <= max) {
+
+      let selectedResponses = []
+
+      question.forEach((q, index) => {
+        if (q === 1) {
+          selectedResponses.push({
+            question_id: b.question_id,
+            response_id: b.choices[index].response_id,
+            ballot_id: ballotId
+          })
+        }
+      })
+
+      if (selectedResponses.length > 0) {
+        selections.push(...selectedResponses)
+      }
+    }
+  })
+
+  return selections
+}
+
+// Method that checks that the browser supports the HTML5 File API
+function browserSupportFileUpload() {
+  var isCompatible = false;
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    isCompatible = true;
+  }
+  return isCompatible;
+}
+
+
+
 $(document).ready(function () {
   const query = window.location.search;
   const params = new URLSearchParams(query);
   const campaignId = params.get("campaign_id");
 
+  // Get the campaign information
   xhr("get", `http://localhost:3000/api/campaign/info/${campaignId}`, {}).done(
     function (json) {
       const campaign = json[0];
@@ -82,12 +140,56 @@ $(document).ready(function () {
     }
   );
 
+  function loadBallots(evt, questions) {
+    if (!browserSupportFileUpload()) {
+      alert("The File APIs are not fully supported in this browser!");
+    } else {
+      let data;
+      let file = evt.target.files[0];
+      let reader = new FileReader();
+      reader.readAsText(file);
+  
+      const selected = []
+  
+      reader.onload = function (event) {
+        let csvData = event.target.result;
+        data = $.csv.toArrays(csvData);
+        if (data.length > 0) {
+          data.forEach(i => {
+            const ballot = validatePaperBallot(questions, i)
+            if (ballot.length > 0) {
+              selected.push(...ballot)
+            }
+          })
+  
+          const body = {
+            campaign_id: campaignId,
+            selections: selected
+          }
+  
+          xhr("post", `http://localhost:3000/api/ballot/import`, body).done(function(response) {
+            if (confirm(response.message)) {
+              window.location.reload()
+            }
+          })  
+        }
+      };
+      reader.onerror = function () {
+        alert("Unable to read " + file.fileName);
+      };
+  
+    }
+  }
+
+  // Get the questions
   xhr(
     "get",
     `http://localhost:3000/api/campaign/results/${campaignId}`,
     {}
   ).done(function (json) {
     const formattedQuestions = formatQuestions(json);
+
+    document.getElementById("ballots").addEventListener("change", (event) => loadBallots(event, formattedQuestions), false)
 
     // Create the initial divs for each position/question
     formattedQuestions.forEach((element) => {
